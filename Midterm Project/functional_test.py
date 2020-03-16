f50f7787c84c43275a7cfff84916e13998c74559
KY-2187 <58202175+KY-2187@users.noreply.github.com>
2020-03-16 17:28:17 +0800

    Update functional_test.py

--- Midterm Project/functional_test.py
+++ Midterm Project/functional_test.py
@@ -10,7 +10,93 @@ class NewVisitorTest(unittest.TestCase):
 	def tearDown(self):
 		self.browser.quit()
 
-	def somethingsomething(self):
+	def test_displays_ingredients_list(self):
+		self.browser.get('http://localhost:8000/ingredients_list')
+
+		self.assertIn('Ingredients - List', self.browser.title)
+
+		self.assertIn(self.browser.current_url,'https://localhost:8000/ingredients_list')
+
+	def test_displays_ingredients_detail(self):
+		self.browser.get('http://localhost:8000/ingredients_detail')
+
+		self.assertIn('Ingredients - Detail', self.browser.title)
+
+		self.assertIn(self.browser.current_url,'https://localhost:8000/ingredients_detail')
+
+	def test_displays_ingredients_update(self):
+		self.browser.get('http://localhost:8000/ingredients_update')
+
+		self.assertIn('Ingredients - Update', self.browser.title)
+
+		self.assertIn(self.browser.current_url,'https://localhost:8000/ingredients_update')
+
+	def test_displays_ingredients_create(self):
+		self.browser.get('http://localhost:8000/ingredients_create')
+
+		self.assertIn('Ingredients - Detail', self.browser.title)
+
+		self.assertIn(self.browser.current_url,'https://localhost:8000/ingredients_create')
+
+	def test_displays_recipes_list(self):
+		self.browser.get('http://localhost:8000/recipes_list')
+
+		self.assertIn('Recipes - List', self.browser.title)
+
+		self.assertIn(self.browser.current_url,'https://localhost:8000/recipes_list')
+
+	def test_displays_recipes_detail(self):
+		self.browser.get('http://localhost:8000/recipes_detail')
+
+		self.assertIn('Recipes - Detail', self.browser.title)
+
+		self.assertIn(self.browser.current_url,'https://localhost:8000/recipes_detail')
+
+	def test_displays_recipes_update(self):
+		self.browser.get('http://localhost:8000/recipes_update')
+
+		self.assertIn('Recipes - Update', self.browser.title)
+
+		self.assertIn(self.browser.current_url,'https://localhost:8000/recipes_update')
+
+	def test_displays_recipes_create(self):
+		self.browser.get('http://localhost:8000/recipes_create')
+
+		self.assertIn('Recipes - Create', self.browser.title)
+
+		self.assertIn(self.browser.current_url,'https://localhost:8000/recipes_create')
+
+	def test_displays_orders_list(self):
+		self.browser.get('http://localhost:8000/orders_list')
+
+		self.assertIn('Orders - List', self.browser.title)
+
+		self.assertIn(self.browser.current_url,'https://localhost:8000/orders_list')
+
+	def test_displays_orders_detail(self):
+		self.browser.get('http://localhost:8000/orders_detail')
+
+		self.assertIn('Orders - Detail', self.browser.title)
+
+		self.assertIn(self.browser.current_url,'https://localhost:8000/orders_detail')
+
+	def test_displays_orders_update(self):
+		self.browser.get('http://localhost:8000/orders_update')
+
+		self.assertIn('Orders - Update', self.browser.title)
+
+		self.assertIn(self.browser.current_url,'https://localhost:8000/orders_update')
+
+	def test_displays_orders_create(self):
+		self.browser.get('http://localhost:8000/orders_create')
+
+		self.assertIn('Orders - Create', self.browser.title)
+
+		self.assertIn(self.browser.current_url,'https://localhost:8000/orders_create')
+		
+
+		self.fail('Finish the test!')
+
 
 
 if __name__ == '__main__':
